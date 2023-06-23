769294e24269a83961be17848d4d52c1b2c9bd2d
Noppadon Piya-isarakul <npiya-is@z3t7c4.42bangkok.com>
2023-06-23 16:53:14 +0700

    solve consonantvalue

---
+++ ConsonantValue.go
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ConsonantValue.go                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npiya-is <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/21 15:42:25 by npiya-is          #+#    #+#             */
+/*   Updated: 2023/06/23 16:52:55 by npiya-is         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func solve(str string) uint {
+	vowels := "aeiou"
+	var sum uint
+	var presum uint
+	for _, val := range str {
+		if !(bytes.IndexByte([]byte(vowels), byte(val)) >= 0) {
+			sum += uint(val - ('a' - 1))
+			if sum > presum {
+				presum = sum
+			}
+		} else {
+			sum = 0
+		}
+	}
+	return presum
+}
+
+func main() {
+	fmt.Println(solve("testsolve"))
+}
